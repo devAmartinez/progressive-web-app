ea7f9ba33074332c025712763bcd6881db39a414
Jhonatan Acevedo Martínez <mravast.martinez@gmail.com>
2018-02-27 18:04:45 -0500

    Se añade la ruta para crear nuevas listas

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,27 @@
 // Componentes propios
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { ListCreatorComponent } from './lists/creator/list.creator.component';
 import { AuthGuardService } from './guards/auth.guard.service';
 
 export const appRoutes = [
-	{path: '', component: HomeComponent, pathMatch: 'full', canActivate: [AuthGuardService]},
-	{path: 'login', component: LoginComponent},
-	{path: '**', component: HomeComponent}
+	{
+		path: '',
+		component: HomeComponent,
+		pathMatch: 'full',
+		canActivate: [AuthGuardService]
+	},
+	{
+		path: 'login',
+		component: LoginComponent
+	},
+	{
+		path: 'new',
+		component: ListCreatorComponent,
+		canActivate: [AuthGuardService]
+	},
+	{
+		path: '**',
+		component: HomeComponent
+	}
 ];
