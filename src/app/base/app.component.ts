8b5b5389ca2babf83df46d5a72be02e263f370db
Jhonatan Acevedo Martínez <mravast.martinez@gmail.com>
2018-02-27 17:22:36 -0500

    Implementación de funcionalidad cerrar sesión

--- src/app/base/app.component.ts
+++ src/app/base/app.component.ts
@@ -1,10 +1,23 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styles: []
 })
-export class AppComponent {
 
+export class AppComponent {
+	constructor(
+		private _router : Router,
+		public afAuth : AngularFireAuth
+	) {}
+
+	logout() {
+		this.afAuth.auth.signOut()
+			.then(() => {
+				this._router.navigate(['/login']);
+			});
+	}
 }
