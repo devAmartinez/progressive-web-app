e3ccfba3526b1279818b1cf7335f50c4df2ecb0d
Jhonatan Acevedo Martínez <mravast.martinez@gmail.com>
2018-03-02 11:39:36 -0500

    Método observable getFromList
    Método asincrono para consultar la lista de los pendientes y posteriormente mostrar en la vista

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 import { 
 	AngularFirestore,
-	AngularFirestoreCollection
+	AngularFirestoreCollection,
+	DocumentChangeAction
 } from 'angularfire2/firestore';
 import { ITodo } from '../structures/todos';
 import * as firebase from 'firebase/app';
@@ -11,6 +12,7 @@ import * as firebase from 'firebase/app';
 
 export class TodoService {
 	private _collection : AngularFirestoreCollection<ITodo>;
+	private _ref : Observable<DocumentChangeAction[]>;
 	private _listId : string;
 
 	constructor(
@@ -20,6 +22,21 @@ export class TodoService {
 	setCollection(listId : string) {
 		this._listId = listId;
 		this._collection = this._afs.collection('lists').doc(listId).collection('todos');
+
+		this._ref = this._collection.snapshotChanges();
+	}
+
+	getFromList(listId : string) : Observable<ITodo[]> {
+		if(!this._collection || this._listId != listId) this.setCollection(listId);
+
+		return this._ref.map(actions => {
+			return actions.map(item => {
+				const data = item.payload.doc.data() as ITodo;
+				const id = item.payload.doc.id;
+				
+				return {...data, id};
+			});
+		});
 	}
 
 	add(listId : string, todo : ITodo) : Promise<any> {
