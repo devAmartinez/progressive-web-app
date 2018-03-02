bda0ab281ffea438288a5ed6b99f6d355483e6e0
Jhonatan Acevedo Martínez <mravast.martinez@gmail.com>
2018-03-02 17:37:27 -0500

    Se añade la animación para las card
    La animación consta de al dar click sostenido, se desplaza la tarjeta levemente hacía la izquierda y se prepara la primera parte de desplazar completamente la tarjeta a la izquierda

--- src/app/todos/card/todo.card.component.ts
+++ src/app/todos/card/todo.card.component.ts
@@ -4,18 +4,48 @@ import {
 	Input
 } from '@angular/core';
 import { ITodo } from '../../structures/todos';
+import {
+	trigger,
+	state,
+	style,
+	transition,
+	animate
+} from '@angular/animations';
 
 @Component({
 	selector: 'app-card',
-	templateUrl: './todo.card.component.html'
+	templateUrl: './todo.card.component.html',
+	animations: [
+		trigger('pressAnimation', [
+			state('up, void', style({
+				transform: 'translateX(0)'
+			})),
+			state('down', style({
+				transform: 'translateX(-100px)'
+			})),
+			transition('up <=> down', [
+				animate(100, style({
+					transform: 'translateX(0)'
+				})),
+				animate(100)
+			])
+		])
+	]
 })
 
 export class CardComponent implements OnInit {
 
 	@Input() todo : ITodo;
+	public press : string;
 
-	constructor() {}
+	constructor() {
+		this.press = 'up';
+	}
 
 	ngOnInit() {}
+
+	completed() {
+		console.log('swiped');
+	}
 
 }
