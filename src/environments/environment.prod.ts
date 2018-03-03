22c64908e164db20d431acdc5ab7075297f6201f
Jhonatan Acevedo Martínez <mravast.martinez@gmail.com>
2018-03-03 18:39:03 -0500

    Configuración de firebase para ambiente de producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCdlfwq5SbUSrSLxSmFjxBxUwXhCVOj3O4",
+    authDomain: "progressive-web-app-ce9f3.firebaseapp.com",
+    databaseURL: "https://progressive-web-app-ce9f3.firebaseio.com",
+    projectId: "progressive-web-app-ce9f3",
+    storageBucket: "progressive-web-app-ce9f3.appspot.com",
+    messagingSenderId: "498802918253"
+  }
 };
