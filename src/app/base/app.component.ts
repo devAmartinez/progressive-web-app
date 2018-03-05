fba24ecedad75deff9dbc4450b67342eb213e812
Jhonatan Acevedo Martínez <mravast.martinez@gmail.com>
2018-03-05 15:06:10 -0500

    Se añade el bloque para notificaciones push
    Se le da la posibilidad al usuario, mediante un icono, que indique si va a querer o no las notificaciones push de la aplicaicón

--- src/app/base/app.component.ts
+++ src/app/base/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { PushNotificationService } from '../services/push.notifications.service';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,23 @@ import { AngularFireAuth } from 'angularfire2/auth';
 })
 
 export class AppComponent {
+	public token : boolean;
+
 	constructor(
 		private _router : Router,
-		public afAuth : AngularFireAuth
-	) {}
+		public afAuth : AngularFireAuth,
+		public pushService : PushNotificationService
+	) {
+		this.token = false;
+	}
+
+	requestPushPermision() {
+		this.pushService.requestPermission();
+	}
+
+	rejectPushPermision() {
+		
+	}
 
 	logout() {
 		this.afAuth.auth.signOut()
