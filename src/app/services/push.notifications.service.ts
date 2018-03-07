d96c17cc01a565c13967fe411bd0214f2a6d0eda
Jhonatan Acevedo Martínez <mravast.martinez@gmail.com>
2018-03-07 11:42:36 -0500

    Métodos para el manejo de las notificaciones

--- src/app/services/push.notifications.service.ts
+++ src/app/services/push.notifications.service.ts
@@ -1,10 +1,44 @@
 import { Injectable } from '@angular/core';
+
 import * as firebase from 'firebase';
+
+import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Rx';
+
+import { IUser } from '../structures/users';
+import { UserService } from './users.service';
 
 @Injectable()
 
 export class PushNotificationService {
 	public messaging = firebase.messaging();
+
+	// creador del observador para las notificaciones
+	public subject : Subject<any>;
+	public notification : Observable<any>;
+
+	constructor(
+		public _userService : UserService
+	) {
+		this.subject = new Subject();
+		this.notification = this.subject.asObservable();
+	}
+
+	refreshToken() {
+		this.messaging.onTokenRefresh(() => {
+			this.messaging.getToken()
+				.then((token) => {
+					this._userService.addToken(token);
+				});
+		});
+	}
+
+	watchMessages() {
+		this.messaging.onMessage((notification) => {
+			// Informar al observador de cambios
+			this.subject.next(notification);
+		});
+	}
 
 	getSubscription() : Promise<any> {
 		if (!navigator) return;
@@ -40,7 +74,9 @@ export class PushNotificationService {
 		return this.messaging.requestPermission()
 			.then(() => {
 				return this.messaging.getToken();
-			})
+			}).then((token) => {
+				return this._userService.addToken(token);
+			});
 			//.catch();
 	}
 
